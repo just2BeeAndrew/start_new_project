1e147e1d88ae6c29b24f36da6bdb4d9aab24fe53
Andrew Dudal <andrew.dudal.1997@gmail.com>
2024-12-06 15:20:29 +0300

    express Get

--- dist/index.js
+++ dist/index.js
@@ -5,10 +5,40 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = 3000;
-app.get('/', (req, res) => {
-    let helloMessage = 'Hello Incubator!!!';
-    res.send(helloMessage);
+const port = process.env.PORT || 5000;
+const products = [{ id: 1, title: 'tomato' }, { id: 2, title: 'orange' }];
+const adresses = [{ id: 1, value: 'Nezalejnasti 12' }, { id: 2, value: 'Selickaga 11' }];
+app.get('/products', (req, res) => {
+    if (req.query.title) {
+        let searchString = req.query.title.toString();
+        res.send(products.filter(p => p.title.indexOf(searchString) > -1));
+    }
+    else {
+        res.send(products);
+    }
+});
+app.get('/products/:id', (req, res) => {
+    let product = products.find(p => p.id === +req.params.id);
+    if (product) {
+        res.send(product);
+    }
+    else {
+        res.send(404);
+    }
+    res.send(product);
+});
+app.get('/adresses', (req, res) => {
+    res.send(adresses);
+});
+app.get('/adresses/:id', (req, res) => {
+    let adress = adresses.find(a => a.id === +req.params.id);
+    if (adress) {
+        res.send(adress);
+    }
+    else {
+        res.send(404);
+    }
+    res.send(adress);
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,41 @@
 import express, {Request, Response} from 'express'
-const app = express()
-const port = 3000
 
-app.get('/', (req: Request, res: Response) => {
-    let helloMessage = 'Hello Incubator!!!';
-    res.send(helloMessage)
+const app = express()
+
+const port = process.env.PORT || 5000
+
+const products = [{id: 1, title: 'tomato'}, {id: 2, title: 'orange'}]
+const adresses = [{id: 1, value: 'Nezalejnasti 12'}, {id: 2, value: 'Selickaga 11'}]
+
+app.get('/products', (req: Request, res: Response) => {
+    if (req.query.title) {
+        let searchString = req.query.title.toString();
+        res.send(products.filter(p => p.title.indexOf(searchString) > -1))
+    } else {
+        res.send(products)
+    }
+
+})
+app.get('/products/:id', (req: Request, res: Response) => {
+    let product = products.find(p => p.id === +req.params.id);
+    if (product) {
+        res.send(product)
+    } else {
+        res.send(404)
+    }
+    res.send(product)
+})
+app.get('/adresses', (req: Request, res: Response) => {
+    res.send(adresses)
+})
+app.get('/adresses/:id', (req: Request, res: Response) => {
+    let adress = adresses.find(a => a.id === +req.params.id);
+    if (adress) {
+        res.send(adress)
+    } else {
+        res.send(404)
+    }
+    res.send(adress)
 })
 
 app.listen(port, () => {
